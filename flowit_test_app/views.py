7062ae312ca1a6a134a32c3d3b6fdae1da7aa91d
Andrievs Niedra <andrievs.niedra@inspired.lv>
2021-07-06 00:55:33 +0300

    simple controllers and views for orders

--- flowit_test_app/views.py
+++ flowit_test_app/views.py
@@ -1,3 +1,15 @@
+from .models import Order
+from django.http import Http404
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    order_list = Order.objects.all()
+    return render(request, 'orders/index.html', {'order_list': order_list,})
+
+def detail(request, order_id):
+    try:
+        order = Order.objects.get(pk=order_id)
+        product_list = order.orderproduct_set.all()
+    except Order.DoesNotExist:
+        raise Http404("Order does not exist")
+    return render(request, 'orders/detail.html', {'order': order,'product_list':product_list,})
