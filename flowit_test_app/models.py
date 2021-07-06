52bee55bde64a8ea9d9417dc58caf4fa3a1bae9c
Andrievs Niedra <andrievs.niedra@inspired.lv>
2021-07-06 11:24:12 +0300

    add bracode generation

--- flowit_test_app/models.py
+++ flowit_test_app/models.py
@@ -2,6 +2,11 @@ from django.core.validators import validate_email
 from django.utils import timezone
 from django.db import models
 
+import barcode
+from barcode.writer import ImageWriter
+from io import BytesIO
+from django.core.files import File
+
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100, validators=[validate_email])
@@ -12,9 +17,17 @@ class Employee(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     barcode = models.CharField(max_length=50)
+    barcode_image = models.ImageField(upload_to='barcodes/', blank=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        COD128 = barcode.get_barcode_class('code128')
+        rv = BytesIO()
+        code = COD128(f'{self.barcode}', writer=ImageWriter()).write(rv)
+        self.barcode_image.save(f'{self.id}.png', File(rv), save=False)
+        return super().save(*args, **kwargs)
 
 class Customer(models.Model):
     name = models.CharField(max_length=100)
