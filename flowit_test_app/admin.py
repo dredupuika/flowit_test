f4de4bed3a58d1ca5e11d700f5dbf5dd759396db
Andrievs Niedra <andrievs.niedra@inspired.lv>
2021-07-06 11:11:58 +0300

    Update admin.py

--- flowit_test_app/admin.py
+++ flowit_test_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Employee, Product, Customer, Order, OrderProduct
+
+admin.site.register(Employee)
+admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderProduct)
