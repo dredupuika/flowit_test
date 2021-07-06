d8db2a34e40f0985e564000efd98337a7a5df2fd
Andrievs Niedra <andrievs.niedra@inspired.lv>
2021-07-06 16:49:54 +0300

    fix url names

--- flowit_test_app/urls.py
+++ flowit_test_app/urls.py
@@ -4,11 +4,11 @@ from . import order_views
 from . import product_views
 
 urlpatterns = [
-    path('orders/', order_views.index, name='index'),
-    path('orders/<int:order_id>/', order_views.detail, name='detail'),
-    path('orders/<int:order_id>/pdf', order_views.pdf, name='pdf'),
-    path('orders/<int:order_id>/email_pdf', order_views.email_pdf, name='email_pdf'),
+    path('orders/', order_views.index, name='orders_index'),
+    path('orders/<int:order_id>/', order_views.detail, name='orders_detail'),
+    path('orders/<int:order_id>/pdf', order_views.pdf, name='orders_pdf'),
+    path('orders/<int:order_id>/email_pdf', order_views.email_pdf, name='orders_email_pdf'),
 
-    path('products/', product_views.index, name='index'),
-    path('products/<int:product_id>/', product_views.detail, name='detail'),
+    path('products/', product_views.index, name='products_index'),
+    path('products/<int:product_id>/', product_views.detail, name='products_detail'),
 ]
